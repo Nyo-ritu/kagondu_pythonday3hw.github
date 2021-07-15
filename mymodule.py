b45d63f3ad4ec2fcbba72bc34814567b54322855
nyo-ritu <kagondu.wai@gmail.com>
2021-07-15 09:26:51 -0400

    first commit

---
+++ mymodule.py
@@ -0,0 +1,18 @@
+def area4():
+    user_i = int(input("What's the length of your house? give me a number in feet (just type the number): "))
+    user_i2 = int(input("What's the width? give me a number in feet (just type the number: "))
+    an_1 = user_i2 * user_i
+    return f'the area of this house is {an_1} square foot'
+
+
+
+
+
+
+
+def circum():
+    user_in = int(input("What's the radius of your house? Give me a number in feet (just type the number): "))
+    blah_2 = user_in * 2 * 3.14
+    return f'the circumference of this house is {blah_2}'
+
+
